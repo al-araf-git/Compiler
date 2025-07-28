4b0f7389373f89166f5615cbc087fd22b246fe5d
Mohammad Sadiq Al Araf <79009629+al-araf-git@users.noreply.github.com>
2025-07-28 10:30:45 +0600

    Add files via upload

--- LabTask3.cpp
+++ LabTask3.cpp
@@ -1,48 +1,66 @@
 #include <iostream>
 #include <string>
-#include <cctype> // for isspace
+#include <cctype>
+#include <fstream>
 
 using namespace std;
 
-
-void checkWords(string words[], int spaceCount)
+void checkWords(string words[], int wordCount)
 {
     string keyword[85] = {
-    "alignas", "alignof", "and", "and_eq", "asm", "auto",
-    "bitand", "bitor", "bool", "break", "case", "catch",
-    "char", "char16_t", "char32_t", "class", "compl", "const",
-    "constexpr", "const_cast", "continue", "decltype", "default",
-    "delete", "do", "double", "dynamic_cast", "else", "enum",
-    "explicit", "export", "extern", "false", "float", "for",
-    "friend", "goto", "if", "inline", "int", "long", "mutable",
-    "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
-    "operator", "or", "or_eq", "private", "protected", "public",
-    "register", "reinterpret_cast", "return", "short", "signed",
-    "sizeof", "static", "static_assert", "static_cast", "struct",
-    "switch", "template", "this", "thread_local", "throw", "true",
-    "try", "typedef", "typeid", "typename", "union", "unsigned",
-    "using", "virtual", "void", "volatile", "wchar_t", "while",
-    "xor", "xor_eq", "string"
+        "alignas", "alignof", "and", "and_eq", "asm", "auto",
+        "bitand", "bitor", "bool", "break", "case", "catch",
+        "char", "char16_t", "char32_t", "class", "compl", "const",
+        "constexpr", "const_cast", "continue", "decltype", "default",
+        "delete", "do", "double", "dynamic_cast", "else", "enum",
+        "explicit", "export", "extern", "false", "float", "for",
+        "friend", "goto", "if", "inline", "int", "long", "mutable",
+        "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
+        "operator", "or", "or_eq", "private", "protected", "public",
+        "register", "reinterpret_cast", "return", "short", "signed",
+        "sizeof", "static", "static_assert", "static_cast", "struct",
+        "switch", "template", "this", "thread_local", "throw", "true",
+        "try", "typedef", "typeid", "typename", "union", "unsigned",
+        "using", "virtual", "void", "volatile", "wchar_t", "while",
+        "xor", "xor_eq", "string"
     };
+
     string opertate[6] = {"+", "-", "*", "/", "++", "--"};
 
-    for(int i = 0; i < spaceCount; i++)
+    for (int i = 0; i < wordCount; i++)
     {
-        if(words[i] == keyword[i])
+        bool isKeyword = false;
+        bool isOperator = false;
+
+        for (int k = 0; k < 85; k++)
         {
-            cout<<"[KEYWORD]: " << words[i]<<endl;
+            if (words[i] == keyword[k])
+            {
+                isKeyword = true;
+                break;
+            }
         }
-        else if(words[i] == opertate[i])
+
+        if (!isKeyword)
         {
-            cout<<"[OPERATOR]: " << words[i]<<endl;
+            for (int o = 0; o < 6; o++)
+            {
+                if (words[i] == opertate[o])
+                {
+                    isOperator = true;
+                    break;
+                }
+            }
         }
-        else{
-            cout<<"[INVALID]: " <<words[i]<<endl;
-        }
+
+        if (isKeyword)
+            cout << "[KEYWORD]: " << words[i] << endl;
+        else if (isOperator)
+            cout << "[OPERATOR]: " << words[i] << endl;
+        else
+            cout << "[INVALID]: " << words[i] << endl;
     }
-
 }
-
 
 void codeDivider(const string& code)
 {
@@ -59,7 +77,6 @@ void codeDivider(const string& code)
 
     int wordCount = spaceCount + 1;
 
-    // Dynamically allocate array of strings
     string* words = new string[wordCount];
 
     string temp = "";
@@ -82,27 +99,51 @@ void codeDivider(const string& code)
         }
     }
 
-
     if (!temp.empty())
     {
         words[tempCount] = temp;
         tempCount++;
     }
 
-
-    for (int i = 0; i < tempCount; i++)
-    {
-        cout << words[i] << endl;
-    }
-
     checkWords(words, tempCount);
 
+    delete[] words;
+}
+
+string readFileContent(const string& filename)
+{
+    ifstream inputFile(filename);
+    if (!inputFile)
+    {
+        cerr << "Error opening file: " << filename << endl;
+        return "";
+    }
+
+    string content;
+    string line;
+
+    while (getline(inputFile, line))
+    {
+        content += line + '\n';
+    }
+
+    inputFile.close();
+    return content;
 }
 
 void codeWritting()
 {
-    string code = "string name;";
-    codeDivider(code);
+    string filename = "test.txt";
+    string code = readFileContent(filename);
+
+    if (!code.empty())
+    {
+        codeDivider(code);
+    }
+    else
+    {
+        cout << "No code to process." << endl;
+    }
 }
 
 int main()
